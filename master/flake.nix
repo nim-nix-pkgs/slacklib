85214ba5de72c1e7358b54c9c0f11302d08029fd
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:39 -0300

    chore: re-index ../nimpkgs/s/slacklib

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Library for working with a slack app or sending messages to a slack channel (slack.com)'';
+  description = ''Library for working with a slack app or sending messages to a slack channel'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-slacklib-master.flake = false;
   inputs.src-slacklib-master.owner = "ThomasTJdev";
-  inputs.src-slacklib-master.ref   = "refs/heads/master";
+  inputs.src-slacklib-master.ref   = "master";
   inputs.src-slacklib-master.repo  = "nim_slacklib";
   inputs.src-slacklib-master.type  = "github";
   
